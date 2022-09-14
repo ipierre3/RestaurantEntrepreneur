95744baeea1e68502bfb3d3ed32e4bfe333f1401
Ignace Pierre <ipierre354@gmail.com>
2022-09-14 12:21:59 -0400

    Fix: Corrected bug in logger.py logic causing program to fail to recognize class object methods

--- logger.py
+++ logger.py
@@ -1,16 +1,19 @@
+from order import Order
+from franchise import Franchise
+
 class Logger:
 
     def __init__(self):
         self.transaction_count = 0
         self.daily_sales = 0
 
-    def log_transaction(self, Order, location_number):
+    def log_transaction(self):
         
         self.transaction_count += 1
-        self.daily_sales += int(Order)
+        self.daily_sales += int(Order.price)
 
         self.log = open("log.txt", "a")
-        self.log.write(f"TRX#{self.transaction_count}: {Order} at location {location_number} - ${Order}. Total: ${self.daily_sales}")
+        self.log.write(f"TRX#{self.transaction_count}: {Order.dish_name} at location {Franchise.location_number} - ${Order.price}. Total: ${self.daily_sales}")
         self.log.close()
 
 logger = Logger()
